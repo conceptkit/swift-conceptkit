ca3b26661ceaa96a59791209a2078f527e6a8351
Mitch <mitchrob@icloud.com>
2025-04-19 10:54:17 -0400

    simplified extensions

--- Sources/ConceptKit/Concept.swift
+++ Sources/ConceptKit/Concept.swift
@@ -1,14 +1,13 @@
 public typealias ConceptID = String
 public typealias ConceptIDPath = [ConceptID]
-public typealias ConceptGraph = [ConceptID: Concept]
 
-public struct Concept: Codable {
+public struct Concept {
     public let id: ConceptID
     public let vectors: [Vector]
 }
 
-public struct Vector: Codable, Hashable {
-    public enum Operator: String, Codable, Hashable, CaseIterable {
+public struct Vector: Hashable {
+    public enum Operator: String {
         case feed, add, diff, diffabs, multiply, multiplyint, divide, divideint
         case equalTo, notEqualTo, greaterThan, lessThan, lessThanOrEqualTo
     }
@@ -18,3 +17,5 @@ public struct Vector: Codable, Hashable {
     public let operand: ConceptIDPath?
     public let operat0r: Operator
 }
+
+public typealias ConceptGraph = [ConceptID: Concept]
